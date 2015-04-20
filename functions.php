2f67a9427eaf110fb27aa3edbff0368bbf297666
Peter B Smith <peterbutlersmith@gmail.com>
2015-04-20 14:52:28 -0400

    adding summary page skeleton

--- functions.php
+++ functions.php
@@ -5,6 +5,8 @@
 $content = $_GET['content'];
 $startDay = $_GET['startDay'];
 $endDay = $_GET['endDay'];
+$startSummary = $_GET['startSummary'];
+$endSummary = $_GET['endSummary'];
 
 //POST
 $postContent = $_POST['content'];
@@ -40,6 +42,29 @@ function getDayJSON($startDay, $endDay){
      $result = $db->prepare("Select * FROM pillars_log WHERE event_date between ? and ?;");
      $result->bindParam(1, $startDay);
      $result->bindParam(2, $endDay);
+     $result->execute();
+  } catch (Exception $e){
+    echo $e->getMessage();
+    die();
+  }
+
+
+  $rows = array();
+
+  while($r = $result->fetch(PDO::FETCH_ASSOC)) {
+    $rows[] = $r;
+  }
+
+  echo json_encode($rows);
+}
+
+function getSummaryJSON($startSummary, $endSummary){
+  require_once 'db/connect.php';
+
+  try {
+     $result = $db->prepare("Select * FROM pillars_days WHERE event_date between ? AND ?;");
+     $result->bindParam(1, $startSummary);
+     $result->bindParam(2, $endSummary);
      $result->execute();
   } catch (Exception $e){
     echo $e->getMessage();
@@ -124,6 +149,7 @@ function updateEntry($id, $duration, $pillar){
 //Content
 //if($content == "lastEntry"){ getLastEntryJSON(); }
 if($content == "today"){ getDayJSON($startDay,$endDay); }
+if($content == "summary"){ getSummaryJSON($startSummary,$endSummary); }
 if($postContent == "newEntry"){ newEntryJSON($pillar, $date, $duration, $quality, $notes); }
 if($postContent == "addDay"){addDay($date,$quality,$notes);}
 if($postContent == "updateEntry") {updateEntry($id, $duration, $pillar);}
---
+++ js/summary.js
@@ -0,0 +1,25 @@
+$("body").on("click", "#go", function(){
+  //update Table
+  console.log("Updating Table...");
+  var startSummary = $('#startSummaryPicker').val();
+  var endSummary = $('#endSummaryPicker').val();
+  buildSummary(startSummary, endSummary);
+});
+
+//add tablesorter to summaryTable
+$("#summaryTable").tablesorter();
+
+//Set Default End Date to Today
+$('#endSummaryPicker').val(new Date().toDateInputValue());
+
+var startSummary = new Date();
+startSummary.setDate(startSummary.getDate() - 6);
+$('#startSummaryPicker').val(startSummary.toDateInputValue());
+
+//Initialize Day Table with Today's Data
+var today = new Date(Date.now());
+var endSummary = today.toJSONLocal();
+today.setDate(today.getDate() - 6);
+var startSummary = today.toJSONLocal();
+
+buildSummary(startSummary, endSummary);
