b65f985a4699aacfdae8c4db02ab92043f9f75ff
Peter B Smith <peterbutlersmith@gmail.com>
2015-06-24 16:16:16 -0400

    v0.4 donut chart exists in Angular directive

--- src/js/daily_donut.js
+++ src/js/daily_donut.js
@@ -1,64 +1,75 @@
-var isX = true;
+var donutApp = angular.module('donutApp',[]);
 
-var x = [10,20,70,40,50,60,30];
-var y = [10,40,30,60,80,10,20];
-var data = x;
+donutApp.directive('donutChart',function(){
+  function link(scope,element,attr){
+    var isX = true;
 
-var height = 500;
-var width = 500;
-var radius = Math.min(width,height) / 2;
+    var x = [10,20,70,40,50,60,30];
+    var y = [10,40,30,null,80,10,20];
+    var data = x;
 
-var svg = d3.select('.donut').append('svg');
+    var height = 500;
+    var width = 500;
+    var radius = Math.min(width,height) / 2;
 
-svg
-  .attr({
-    'height': height,
-    'width': width
-  });
+    var svg = d3.select(element[0]).append('svg');
 
-var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    svg
+      .attr({
+        'height': height,
+        'width': width
+      });
 
-var arc = d3.svg.arc()
-  .innerRadius(radius * 0.5)
-  .outerRadius(radius * 0.9);
+    var color = d3.scale.ordinal()
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-var pie = d3.layout.pie().sort(null);
+    var arc = d3.svg.arc()
+      .innerRadius(radius * 0.5)
+      .outerRadius(radius * 0.9);
+
+    var pie = d3.layout.pie().sort(null);
 
 
-var donut = svg.append('g')
-  .attr('transform','translate(' + width/2 + ',' + height/2 + ')');
+    var donut = svg.append('g')
+      .attr('transform','translate(' + width/2 + ',' + height/2 + ')');
 
-var arcs = donut.selectAll('path')
-  .data(pie(data))
-  .enter()
-    .append('path')
-      .attr('fill',function(d,i){return color(i);})
-      .each(function(d) {this._current = d;});
+    var arcs = donut.selectAll('path')
+      .data(pie(data))
+      .enter()
+        .append('path')
+          .attr('fill',function(d,i){return color(i);})
+          .each(function(d) {this._current = d;});
 
-arcs.data(pie(data)).attr('d',arc);
+    arcs.data(pie(data)).attr('d',arc);
 
-function arcTween(a){
-  var i = d3.interpolate(this._current, a);
-  this._current = i(0);
-  return function(t) {
-    return arc(i(t));
+    function arcTween(a){
+      var i = d3.interpolate(this._current, a);
+      this._current = i(0);
+      return function(t) {
+        return arc(i(t));
+      };
+    }
+
+    function update(){
+      isX = !isX;
+
+      if(isX){
+        data = x;
+      }
+      else {
+        data = y;
+      }
+
+      arcs.data(pie(data)).transition().duration(500).ease('linear').attrTween('d',arcTween);
+
+    }
+
+
+
+    setInterval(update,2000);
+  }
+  return {
+    link: link,
+    restrict: 'E'
   };
-}
-
-function update(){
-  isX = !isX;
-
-  if(isX){
-    data = x;
-  }
-  else {
-    data = y;
-  }
-
-  arcs.data(pie(data)).transition().duration(500).ease('linear').attrTween('d',arcTween);
-
-}
-
-
-setInterval(update,2000);
+});
