8bdbf09acad6c42fb949bbac0eb68c25665c9cd1
Peter B Smith <peterbutlersmith@gmail.com>
2015-06-24 15:14:29 -0400

    v0.2 Donut Chart updates after interval w/ same # slices

--- src/js/daily_donut.js
+++ src/js/daily_donut.js
@@ -1,4 +1,9 @@
+var isX = true;
+
 var x = [10,20,70,40,50,60,30];
+var y = [10,40,30,60,80,10,20];
+var data = x;
+
 var height = 500;
 var width = 500;
 var radius = Math.min(width,height) / 2;
@@ -25,8 +30,35 @@ var donut = svg.append('g')
   .attr('transform','translate(' + width/2 + ',' + height/2 + ')');
 
 var arcs = donut.selectAll('path')
-  .data(pie(x))
+  .data(pie(data))
   .enter()
     .append('path')
       .attr('fill',function(d,i){return color(i);})
-      .attr('d',arc);
+      .each(function(d) {this._current = d;});
+
+arcs.data(pie(data)).attr('d',arc);
+
+function arcTween(a){
+  var i = d3.interpolate(this._current, a);
+  this._current = i(0);
+  return function(t) {
+    return arc(i(t));
+  };
+}
+
+function update(){
+  isX = !isX;
+
+  if(isX){
+    data = x;
+  }
+  else {
+    data = y;
+  }
+
+  arcs.data(pie(data)).transition().duration(500).ease('linear').attrTween('d',arcTween);
+
+}
+
+
+setInterval(update,2000);
