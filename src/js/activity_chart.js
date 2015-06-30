8bb6f4b42ec0e678a75b5b3bef3b251550dd92b5
Peter B Smith <peterbutlersmith@gmail.com>
2015-06-29 20:56:33 -0400

    chart initializes to current day and updates on change using public and private functions on controller

--- src/js/activity_chart.js
+++ src/js/activity_chart.js
@@ -1,15 +1,23 @@
 var activityApp = angular.module('activityApp',[]);
 
 activityApp.controller('ActivityCtrl',['$scope','$http',function($scope,$http){
-  $http.get('data1.json').success(function(response){
-    $scope.activityData = response;
-  });
+  $scope.today = new Date(Date.now());
+  var todayJSON = $scope.today.toJSONLocal();
 
-  $scope.update = function(){
-    $http.get('data.json').success(function(response){
+  var update = function(url){
+    $http.get(url).success(function(response){
       $scope.activityData = response;
     });
   };
+
+  update('functions.php?content=pillarsLog&startDay='+ todayJSON +'&endDay='+todayJSON+'T23%3A59%3A59');
+
+  $scope.newDay = function(){
+    todayJSON = $scope.today.toJSONLocal();
+    update('http://localhost/pillars/dev/functions.php?content=pillarsLog&startDay='+todayJSON+'&endDay='+todayJSON+'T23%3A59%3A59');
+  };
+
+
 }]);
 
 activityApp.directive('activityChart',function(){
