7a03cce9f8be164a4212e4165a0463902471e171
Peter B Smith <peterbutlersmith@gmail.com>
2015-07-15 15:36:22 -0400

    data explorer v0.5 datetime formatter

--- src/js/data_explorer.js
+++ src/js/data_explorer.js
@@ -57,3 +57,30 @@ dataApp.directive('select',function(){
     }
   };
 });
+
+angular.module('dataAppFilters',[]).filter('datetime',function(){
+  return function(value){
+    //Expects string formatted "YYYY-MM-DDTHH:MM:SS"
+    var y, mm, dd, yyyy, hh, mi;
+    y= value.substr(5,2) + "/" + value.substr(8,2) + "/" + value.substr(0,4) + " ";
+    hh = parseInt(value.substr(11,2),10);
+    mi = value.substr(14,2);
+    if (hh > 12){
+      hh -= 12;
+      y += hh + ":" + mi + " PM";
+      return y;
+    }
+    else if (hh === 0){
+      y += 12 + ":" + mi + " AM";
+      return y;
+    }
+    else if (hh === 12){
+      y += hh + ":" + mi + " PM";
+      return y;
+    }
+    else {
+      y += hh + ":" + mi + " AM";
+      return y;
+    }
+  };
+});
