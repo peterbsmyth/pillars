8d8c022dfa95d260e28d46a74ad8c4738dc94bff
Peter B Smith <peterbutlersmith@gmail.com>
2015-07-15 09:57:40 -0400

    data_expolorer v0.3 tolerant form

--- src/angular_functions.php
+++ src/angular_functions.php
@@ -4,27 +4,102 @@
 */
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
-@$pillar = $request->pillar;
-@$minDuration = $request->minDuration;
-@$maxDuration = $request->maxDuration;
-@$quality = $request->quality;
-@$startDate = $request->startDate;
-@$endDate = $request->endDate;
-@$notes = $request->notes;
 
-function getDataExplorerJSON($pillar,$minDuration,$maxDuration,$quality,$startDate,$endDate,$notes){
+function getDataExplorerJSON($request){
   require_once 'db/connect.php';
-  $notes = "%" . $notes . "%";
+
+  @$pillar = $request->pillar;
+  @$minDuration = $request->minDuration;
+  @$maxDuration = $request->maxDuration;
+  @$quality = $request->quality;
+  @$startDate = $request->startDate;
+  @$endDate = $request->endDate;
+  @$notes = $request->notes;
+  $counter = 0;
+
+  $sql = "SELECt * FROM pillars_log WHERE";
+  if($pillar) {
+    $sql .= " pillar = ? AND ";
+  }
+  if($minDuration && $maxDuration) {
+    $sql .= " duration BETWEEN ? AND ? AND ";
+  }
+  elseif($minDuration && !$maxDuration) {
+    $sql .= " duration >= ? AND ";
+  }
+  elseif(!$minDuration && $maxDuration) {
+    $sql .= " duration <= ? AND ";
+  }
+  if($quality) {
+    $sql .= "quality = ? AND ";
+  }
+  if($startDate == "ull" && $endDate == "ull"){
+
+  }
+  elseif($startDate && $endDate == "ull") {
+    $sql .= "event_date >= ? AND ";
+  }
+  elseif($startDate == "ull" && $endDate) {
+    $sql .= "event_date <= ? AND ";
+  }
+  elseif($startDate && $endDate) {
+    $sql .= "event_date BETWEEN ? AND ? AND ";
+  }
+  if($notes) {
+    $notes = "%" . $notes . "%";
+    $sql .= "notes  LIKE ? AND";
+  }
+  $sql .= " 1=1";
+
+  // echo json_encode($request);
 
   try {
-    $result = $db->prepare("SELECt * FROM pillars_log WHERE pillar = ? AND duration BETWEEN ? AND ? AND quality = ? AND event_date BETWEEN ? AND ? AND notes LIKE ?;");
-    $result->bindParam(1,$pillar);
-    $result->bindParam(2,$minDuration);
-    $result->bindParam(3,$maxDuration);
-    $result->bindParam(4,$quality);
-    $result->bindParam(5,$startDate);
-    $result->bindParam(6,$endDate);
-    $result->bindParam(7,$notes);
+
+    $result = $db->prepare($sql);
+    if($pillar) {
+      $counter++;
+      $result->bindParam($counter,$pillar);
+    }
+    if($minDuration && $maxDuration) {
+      $counter++;
+      $result->bindParam($counter,$minDuration);
+      $counter++;
+      $result->bindParam($counter,$maxDuration);
+    }
+    elseif($minDuration && !$maxDuration) {
+      $counter++;
+      $result->bindParam($counter,$minDuration);
+    }
+    elseif(!$minDuration && $maxDuration) {
+      $counter++;
+      $result->bindParam($counter,$maxDuration);
+    }
+    if($quality) {
+      $counter++;
+      $result->bindParam($counter,$quality);
+    }
+    if($startDate == "ull" && $endDate == "ull"){
+
+    }
+    elseif($startDate && $endDate == "ull") {
+      $counter++;
+      $result->bindParam($counter,$startDate);
+    }
+    elseif($startDate == "ull" && $endDate) {
+      $counter++;
+      $result->bindParam($counter,$endDate);
+    }
+    elseif($startDate && $endDate) {
+      $counter++;
+      $result->bindParam($counter,$startDate);
+      $counter++;
+      $result->bindParam($counter,$endDate);
+    }
+
+    if($notes) {
+      $counter++;
+      $result->bindParam($counter,$notes);
+    }
     $result->execute();
   } catch (Exception $e){
     echo $e->getMessage();
@@ -40,5 +115,5 @@ function getDataExplorerJSON($pillar,$minDuration,$maxDuration,$quality,$startDa
   echo json_encode($rows);
 }
 
-getDataExplorerJSON($pillar,$minDuration,$maxDuration,$quality,$startDate,$endDate,$notes);
+getDataExplorerJSON($request);
 ?>
