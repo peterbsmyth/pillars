1f70a7edec0aeee5927100ffbbe0d6749e11ed91
Peter B Smith <peterbutlersmith@gmail.com>
2015-07-27 11:26:54 -0400

    annual calendar bound to this year's pillars data

--- src/js/controllers/AnnualCalendarCtrl.js
+++ src/js/controllers/AnnualCalendarCtrl.js
@@ -1,4 +1,20 @@
 chartsApp.controller('AnnualCalendarCtrl',['$scope','$http',function($scope,$http){
   $scope.today = new Date(Date.now());
-  $scope.today = $scope.today.getDate();
+  $scope.lastYear = addDays(today,-365);
+  var todayJSON = $scope.today.toJSONLocal();
+  var lastYearJSON = $scope.lastYear.toJSONLocal();
+
+  todayUTC = makeUTCDate(todayJSON);
+  todayJSON = todayUTC.toJSONLocal() + "T23:59:59";
+
+  var update = function(url){
+    $http.get(url).success(function(response){
+      $scope.annualCalendar = {
+          data: response
+      };
+    });
+  };
+
+  update('functions.php?content=pillarsLog&startDay='+ lastYearJSON +'&endDay='+todayJSON+'T23%3A59%3A59');
+
 }]);
--- src/js/directives/annualCalendar.js
+++ src/js/directives/annualCalendar.js
@@ -3,13 +3,16 @@ chartsApp.directive('annualCalendar',function(){
     restrict: 'E',
     scope: {data: '='},
     link: function(scope,element,attrs){
-      var array = [];
+      var calendar = [];
       var today = new Date(Date.now());
       for (i=0; i < 366; i++){
-        array.unshift(today);
+        dateString = today.toJSONLocal();
+        calendar.unshift({
+          date: dateString,
+          count: 0
+        });
         today = addDays(today,-1);
       }
-      console.log(array);
 
       var width = 11 + (365*13);
       var height = 11;
@@ -20,7 +23,7 @@ chartsApp.directive('annualCalendar',function(){
           .append('g');
 
       svg.selectAll('rect')
-        .data(array)
+        .data(calendar)
         .enter()
       .append('rect')
         .attr('width',11)
@@ -28,6 +31,38 @@ chartsApp.directive('annualCalendar',function(){
         .attr('x',function(d,i){return i*13;})
         .attr('fill','#eeeeee');
 
+        scope.$watch('data',function(newVal,oldVal){
+          if (!newVal) return;
+
+          var events = {};
+          var l = newVal.length;
+          while(l--){
+            var currentDate = newVal[l].event_date.substr(0,10);
+            if(!events[currentDate]){
+              events[currentDate] = 0;
+            }
+            events[currentDate]++;
+          }
+
+          for (var i = 0; i < calendar.length; i++) {
+            if (events[calendar[i].date]){
+              calendar[i].count = events[calendar[i].date];
+            }
+          }
+
+          // var data = d3.nest()
+          //     .key(function(d){
+          //       return d.event_date.substr(0,10);
+          //     })
+          //     .rollup(function(v){
+          //       return v.length;
+          //     })
+          //     .entries(newVal);
+              console.log("EVENTS:");
+              console.log(events);
+              console.log("CALENDAR:");
+              console.log(calendar);
+        });
 
     }
   };
