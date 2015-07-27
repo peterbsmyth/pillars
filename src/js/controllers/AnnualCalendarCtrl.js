8bf7db27e6d4587d0d1a473450a7057f11a99023
Peter B Smith <peterbutlersmith@gmail.com>
2015-07-27 10:36:01 -0400

    annual calendar v0.1 366 squares in a line bound to an array of dates ending today

--- src/js/controllers/AnnualCalendarCtrl.js
+++ src/js/controllers/AnnualCalendarCtrl.js
@@ -1,3 +1,4 @@
 chartsApp.controller('AnnualCalendarCtrl',['$scope','$http',function($scope,$http){
-  
+  $scope.today = new Date(Date.now());
+  $scope.today = $scope.today.getDate();
 }]);
--- src/js/directives/annualCalendar.js
+++ src/js/directives/annualCalendar.js
@@ -3,6 +3,31 @@ chartsApp.directive('annualCalendar',function(){
     restrict: 'E',
     scope: {data: '='},
     link: function(scope,element,attrs){
+      var array = [];
+      var today = new Date(Date.now());
+      for (i=0; i < 366; i++){
+        array.unshift(today);
+        today = addDays(today,-1);
+      }
+      console.log(array);
+
+      var width = 11 + (365*13);
+      var height = 11;
+
+      var svg = d3.select(element[0]).append('svg')
+            .attr('width',width)
+            .attr('height',height)
+          .append('g');
+
+      svg.selectAll('rect')
+        .data(array)
+        .enter()
+      .append('rect')
+        .attr('width',11)
+        .attr('height',11)
+        .attr('x',function(d,i){return i*13;})
+        .attr('fill','#eeeeee');
+
 
     }
   };
