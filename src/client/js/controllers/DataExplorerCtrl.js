c6caa5dc636c21cdf5919735c3a6864a3afc345a
Peter B Smith <peterbutlersmith@gmail.com>
2015-11-21 12:10:10 -0500

    MEANing data explorer

--- src/client/js/controllers/DataExplorerCtrl.js
+++ src/client/js/controllers/DataExplorerCtrl.js
@@ -4,23 +4,25 @@ chartsApp.controller('DataExplorerCtrl',['$scope','$http',function($scope,$http)
   $scope.formSubmit = function (){
     var formClone = angular.copy($scope.formData);
 
-    formClone.startDate = (JSON.stringify(formClone.startDate)).substr(1,10);
-
-    formClone.endDate = (JSON.stringify(formClone.endDate)).substr(1,10);
+    // formClone.startDate = (JSON.stringify(formClone.startDate)).substr(1,10);
+    //
+    // formClone.endDate = (JSON.stringify(formClone.endDate)).substr(1,10);
 
     console.log('submitting...');
     $http({
-      method: 'POST',
-      url: 'angular_functions.php',
-      data: formClone,
-      headers: {'Content-type' : 'application/x-www-form-urlencoded'}
+      method: 'GET',
+      url: '/api/query',
+      params: formClone
     }).success(function(response){
+
+      console.log(response);
+
       $scope.success = {
-        data: response.data.entries
+        data: response
       };
-      $scope.stats = {
-        data: response.stats.total_duration
-      };
+      // $scope.stats = {
+      //   data: response.stats.total_duration
+      // };
     });
   };
 }]);
